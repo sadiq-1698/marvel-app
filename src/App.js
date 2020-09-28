ddcebabfe4c81aae451cead880673249033bc123
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-09-28 20:55:49 +0530

    Left with overlap issue

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
   const [currentOffset, setCurrentOffset] = useState(0);
   const [checkedTab, setCheckedTab] = useState('characters');
   const [darkTheme, setDarkTheme] = useState(false);
-  const[isLoading, setIsLoading] = useState(true);
-  const ref = useRef();
-
+  const [isLoading, setIsLoading] = useState(true);
   const[data, setData] = useState([]);
+
+  const ref = useRef();
   
   const constructUrl = (type) => `http://gateway.marvel.com/v1/public/${type}?ts=${TIMESTAMP}&apikey=${PUBLIC_KEY}&hash=${HASH}&offset=${currentOffset}`;
   const URL = constructUrl(checkedTab);
@@ -57,7 +57,7 @@ function App() {
     const fetchData = async () => {
       const response = await axios(URL);
       if(response){
-        ref.current.scrollTop = 0;
+        window.scrollTo(0, 0);
         setData(response.data.data.results);
         setIsLoading(false);
       }
@@ -66,7 +66,7 @@ function App() {
   },[URL, checkedTab, currentOffset, setCurrentOffset]);
   
   const handleTabClick = (tabName, currentTabOffset) => {
-    if(isCurrentTab(tabName))return;
+    if(isCurrentTab(tabName)) return;
     setIsLoading(true);
     setCurrentOffset(currentTabOffset);
   }
@@ -77,49 +77,54 @@ function App() {
   }
 
   return (
-    <div className={`container ${darkTheme ? 'dark-theme': ''}`}>
-        <div className="marvel-logo">
-            <img src={process.env.PUBLIC_URL + '/marvel-logo.png'} alt="marvel-logo" width="150px" height="75px"/>
-            <div className="theme-toggle">
-              <label class="switch"> 
-                <input type="checkbox" checked={darkTheme} onChange={() => setDarkTheme(!darkTheme)}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-        </div>
-        <div className="main-content">
-          <div className="tab-container">
-            <button
-              className={`${currentOffset <= 0 ? 'inactive' : 'prev'}`}
-              disabled={currentOffset <= 0}
-              onClick={() => handlePageChange(false)}
-            >
-              Prev
-            </button>
-            <button
-              className={`${currentOffset >= 1490 ? 'inactive': 'next'}`}
-              disabled={currentOffset >= 1490}
-              onClick={() => handlePageChange()}
-            >
-              Next
-            </button>
+    <div className={`body-container ${darkTheme ? 'dark-theme': ''}`}>
+      <div className={`container ${darkTheme ? 'dark-theme': ''}`}>
+        <div className="header-container">
+          <div className="marvel-logo">
+              <img src={process.env.PUBLIC_URL + '/marvel-logo.png'} alt="marvel-logo" width="150px" height="75px"/>
+              <div className="theme-toggle"><input type="checkbox" id="switch"
+                className="checkbox" checked={darkTheme} onChange={() => setDarkTheme(!darkTheme)}/> 
+                <label htmlFor="switch" className="toggle"> </label> </div>
+              {/* <div className="theme-toggle">
+                <label className="switch"> 
+                  <input type="checkbox" checked={darkTheme} onChange={() => setDarkTheme(!darkTheme)}></input>
+                  <span className="slider round"></span>
+                </label>
+              </div> */}
+          </div>
+          <div className="button-container">
+              <button
+                className={`${currentOffset <= 0 ? 'inactive' : 'prev'} ${darkTheme ? 'dark-theme': ''}`}
+                disabled={currentOffset <= 0}
+                onClick={() => handlePageChange(false)}
+              >
+                Prev
+              </button>
+              <button
+                className={`${currentOffset >= 1490 ? 'inactive': 'next'} ${darkTheme ? 'dark-theme': ''}`}
+                disabled={currentOffset >= 1490}
+                onClick={() => handlePageChange()}
+              >
+                Next
+              </button>
           </div>
           <Tabs checkedTab={checkedTab} handleTabChange={handleTabChange} offset={offset} comicsOffset={comicsOffset} seriesOffset={seriesOffset} />
-            <div className="grid-wrapper" ref={ref}>
-              { 
-                isLoading ? 
-                <div className="loading-container">
-                  <img src={process.env.PUBLIC_URL + '/spinner2.gif'} alt="spinner" width="80px" height="80px"/>       
-                </div> :
-                <div className="grid-container">
-                  { 
-                    data.map((res, key) => (
-                      <Character key={key}  {...res}/>
-                    ))
-                  }      
-                </div> 
-              }
-            </div>
+        </div> 
+        <div className="grid-wrapper" ref={ref}>
+            { 
+              isLoading ? 
+              <div className="loading-container">
+                <img src={process.env.PUBLIC_URL + '/spinner2.gif'} alt="spinner" width="80px" height="80px"/>       
+              </div> :
+              <div className="grid-container">
+                { 
+                  data.map((res, key) => (
+                    <Character key={key}  {...res}/>
+                  ))
+                }      
+              </div> 
+            }
+          </div>
         </div>
     </div>
   );
